c7c5d55b86233a928c7a28c7738be6ab4115bfc9
Saon Islam <saonbd33@gmail.com>
2026-09-11 23:57:50 +0600

    Update SITE_DESCRIPTION for clarity and relevance

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere on your site by using the `import` keyword.
 
 export const SITE_TITLE = "Tech Tips in Fun";
-export const SITE_DESCRIPTION = "No fluff, no sponsored jargon — just honest write-ups from someone who actually uses this stuff every day.";
+export const SITE_DESCRIPTION = "No fluff, no sponsored jargon — just honest write-ups from someone who actually uses techy stuff every day and want to stay ahead of time.";
